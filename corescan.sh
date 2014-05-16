0d28ab4576be361d2cd179a0b43cc73d7190ebc4
alleck <kaleb.lewis92@gmail.com>
2014-05-16 17:51:25 -0500

    Fixed syntax errors

--- corescan.sh
+++ corescan.sh
@@ -6,18 +6,18 @@ echo " | \__/\ (_) | | |  __/\__/ / (_| (_| | | | |           "
 echo -e "  \____/\___/|_|  \___\____/ \___\__,_|_| |_| \033[0m"
 
 
-                                            
+
 ###----> corescan.sh <----###
 
 echo "Finding core files for /home. This may take a while..."
 
 
 
-ls -ld /home/*/public_html/ | awk '{print $9}' > dirs.source
+ls -ld /home*/* | awk '{print $9}' > /var/log/dirs.source
 
 
 #Numbers for loading bar
-percent2=$(wc -l dirs.source | awk '{print $1}')
+percent2=$(wc -l /var/log/dirs.source | awk '{print $1}')
 process=$(bc -l <<< "scale = 3; 100 / $percent2")
 proc2=$process
 
@@ -25,11 +25,11 @@ while read dirs
 do
 
 echo -ne "$process \r"
-        find /home*/$dirs -type f -name "core.[[:digit:]]*[[:digit:]]" >> corefind.log
+        find $dirs -type f -name "core.[[:digit:]]*[[:digit:]]" >> corefind.log
         process=$(bc -l <<< "scale = 2; $process + $proc2")
 
 
-done < dirs.source
+done < /var/log/dirs.source
 
 
 cp corefind.log /home/AcctSize/coreLogs/$(date +%Y%m%d-%H%M)_corefind.log
